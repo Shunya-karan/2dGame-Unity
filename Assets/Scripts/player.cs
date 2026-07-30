3e85d450e56f0180dc19d7054327e6bb13843d5f
shunya-karan <karanyad9072@gmail.com>
2026-07-30 19:00:34 +0530

    player move

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -1,19 +1,32 @@
+using System;
 using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public  Rigidbody2D rb ;
-    public float xInput;
+    private Rigidbody2D rb ; 
+    
+    private float xInput;
+    [SerializeField]private float movementSpeed= 3.5f;
+    [SerializeField]private float jumpForce= 8;
+
+    
+    
     void Start()
     {
-        
+        rb=GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        xInput = Input.GetAxisRaw("Horizontal");
-        
+
+     xInput = Input.GetAxisRaw("Horizontal");
+
+     rb.linearVelocity=new Vector2(xInput*movementSpeed,rb.linearVelocityY);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity=new Vector2(rb.linearVelocityX,jumpForce);
+        }
     }
 }
