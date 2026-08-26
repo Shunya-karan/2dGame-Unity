9f845412a4618394334fecc1b6cb4466626daceb
shunya-karan <karanyad9072@gmail.com>
2026-08-26 17:15:15 +0530

    ChangeColor

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,8 +2,47 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private SpriteRenderer sr;
+    [SerializeField] private float redColorDuration=1;
+    public float timer;
+
+    public float currentTimeInGame;
+    public float lastTimeWasDamage;
+    public void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        ChangeColor();
+    }
+
+    private void ChangeColor()
+    {
+        currentTimeInGame = Time.time;
+
+        if (currentTimeInGame > lastTimeWasDamage + redColorDuration)
+        {
+            if (sr.color != Color.white)
+            {
+                sr.color = Color.white;
+            }
+        }
+    }
+
     public void TakeDamage()
     {
-        Debug.Log(gameObject.name +"Took Damage");
+        // Debug.Log(gameObject.name +"Took Damage");
+
+        sr.color=Color.red;
+        // timer = -redColorDuration;
+        lastTimeWasDamage = Time.time;
+
+    }
+
+    private void TurnWhite()
+    {
+        sr.color = Color.white;
     }
 }
